db982685d0e64583bf00f64597681262b133e9e6
jp4000 <44896784+jp4000@users.noreply.github.com>
2018-12-05 20:18:01 +0500

    Finally, a fix!

--- ratkick.py
+++ ratkick.py
@@ -1,53 +1,48 @@
+# coding: utf-8
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import logging
 from datetime import datetime
 
-filenametime = datetime.now().strftime('%Y-%m-%d_%H.%M.%S_rat.log')
-logging.basicConfig(filename=filenametime, filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.INFO, filename = datetime.now().strftime('%Y-%m-%d_%H.%M.%S_ratkick.log'))
+
+BANNER = '''										
+  _____       _   _  ___      _    
+ |  __ \     | | | |/ (_)    | |   
+ | |__) |__ _| |_| ' / _  ___| | __
+ |  _  // _` | __|  < | |/ __| |/ /
+ | | \ \ (_| | |_| . \| | (__|   < 
+ |_|  \_\__,_|\__|_|\_\_|\___|_|\_\
+                                   
+                                       by: github.com/jp4000'''
 
 def main():
-	login, password = 'LOGIN', 'PASSWORD' # <------- Enter your login and password from vk.com! i wont steal them, i promise.
-	vk_session = vk_api.VkApi(login, password)
-	vk = vk_session.get_api()
-	
-	try:
-		vk_session.auth(token_only=True)
-	except vk_api.AuthError as error_msg:
-		print(error_msg)
-		return
-
-	longpoll = VkLongPoll(vk_session)
-
-	print("Automatic 'rat' kick. Author: vk.com/jp444 / github.com/jp4000")
-	chatid = int(input("ID of a chat: "))					   
-	peerid = chatid + 2000000000						  	   
-	chatname = str(vk.messages.getChat(chat_id=chatid)["title"]) 							 
-	logging.info("Monitoring: " + chatname)					  
-	print("Monitoring: " + chatname)							   
-
-	for event in longpoll.listen():
-		if event.type == VkEventType.MESSAGE_NEW:
-			if event.raw[5] == '':
-				print(event.raw)
-				if event.raw[6]['source_act'] == 'chat_kick_user' or event.raw[6]['source_act'] == 'chat_invite_user':
-					if event.raw[6]['source_mid'] == event.raw[6]['from']:
-						userid = event.raw[6]['from']
-						firstname = vk.users.get(user_ids=userid, fields="")[0]["first_name"]
-						lastname = vk.users.get(user_ids=userid, fields="")[0]["last_name"]
-						callingout = firstname + " " + lastname + " (", userid, ") "
-						try:
-							kickresult = vk.messages.removeChatUser(chat_id=chatid, user_id=userid)
-							if kickresult == 1:
-								logging.info(callingout + "was determined rat and got kicked.")
-								print(callingout + "was determined rat and got kicked.")
-							else:
-								logging.error(callingout + "was determined rat and didnt got kicked because of an error.")
-								print(callingout + "was determined rat and didnt got kicked because of an error.")
-						except:
-								logging.error(callingout + "was determined rat and didnt got kicked because of an error. Printing debug stuff:")
-								logging.error(kickresult, event.raw)
-								print(callingout + "was determined rat and didnt got kicked because of an error.")
-
+    login, password = "LOGIN", "PASSWORD"
+    vk_session = vk_api.VkApi(login, password)
+    vk = vk_session.get_api()
+    try:
+        vk_session.auth(token_only=True)
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+    print(BANNER, end='\n\n')
+    print("What this script does?: It kicks people who left the chat or returned to it. In order for it to work, you must be an administrator of the chat!")
+    chat = int(input("Chat ID, please (Script will crash if str is passed): "))
+    peer = chat + 2000000000
+    print("Monitoring: " + vk.messages.getChat(chat_id=chat)["title"])
+    logging.info("Monitoring: " + vk.messages.getChat(chat_id=chat)["title"])
+    longpoll = VkLongPoll(vk_session)
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.raw[5] == '' and event.raw[3] == peer and event.raw[6]['source_mid'] == event.raw[6]['from'] and event.raw[6]['source_act'] in ('chat_kick_user', 'chat_invite_user'):
+            user = event.raw[6]['from']
+            username = vk.users.get(user_ids=user, fields="")[0]["first_name"], vk.users.get(user_ids=user, fields="")[0]["last_name"]
+            try:
+                kick = vk.messages.removeChatUser(user_id=user, chat_id=chat)
+                print(" ".join(username), user, "was determined rat and got kicked")
+                logging.info(" ".join(username) + " " + user)
+            except:
+                print(" ".join(username), user, "didnt got kicked.")
+                logging.error(" ".join(username) + " " + user)          
 if __name__ == '__main__':
 	main()
+
